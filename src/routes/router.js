181ba905f84c67c0c96d18bfcc8a9be7beab720d
KKKZOZ <70520782+KKKZOZ@users.noreply.github.com>
2023-07-04 20:37:57 +0800

    Fix: router-base

--- src/routes/router.js
+++ src/routes/router.js
@@ -7,6 +7,7 @@ Vue.use(VueRouter);
 // configure router
 const router = new VueRouter({
   mode: 'history',
+  base: '/auto-test/',
   routes, // short for routes: routes
   linkActiveClass: 'active',
   scrollBehavior: (to, from ,savedPosition) => {
